0e78ee9bc9b5cbe1afd46a2bc9a7a71abfd2a563
Luiskof <kof276@gmail.com>
2025-10-16 06:52:24 -0500

    Adjust Image File size

--- src/main/java/com/example/imageanalysisapi/config/WebClientConfig.java
+++ src/main/java/com/example/imageanalysisapi/config/WebClientConfig.java
@@ -3,6 +3,8 @@ package com.example.imageanalysisapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -13,6 +15,13 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl(imaggaApiUrl).build();
+        return builder
+                .baseUrl(imaggaApiUrl)
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(16 * 1024 * 1024))
+                        .build())
+                .build();
     }
 }
